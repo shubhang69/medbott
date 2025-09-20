621cd4d64026d76b278ec99da85bac1d7a52daa5
Shubhang <shubhang.chakrawarty@gmail.com>
2025-09-20 07:09:59 +0000

    i dont want the background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,16 +20,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-[#0A1817]">
-        <div className="relative min-h-screen w-full bg-blurry-gradient overflow-hidden">
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="text-center text-8xl md:text-9xl font-extrabold text-primary/5 select-none space-y-4">
-              <p>MONITOR</p>
-              <p>VIRTUAL</p>
-              <p>ASSISTANT</p>
-              <p>HEALTH</p>
-              <p>CHATBOT</p>
-            </div>
-          </div>
+        <div className="relative min-h-screen w-full">
           {children}
         </div>
         <Toaster />
